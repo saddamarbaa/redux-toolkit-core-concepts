ea13c10f7cf7b6e6b5973780994d88aa3214f1a0
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-08-01 03:44:51 +0700

    first commit

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,23 +1,37 @@
-import classes from './Auth.module.css';
+/** @format */
+
+import classes from "./Auth.module.css";
+import { authActions } from "../store/authSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 const Auth = () => {
-  return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
-  );
+	const dispatch = useDispatch();
+
+	const loginHandler = (event) => {
+		event.preventDefault();
+
+		//  logic go be here
+
+		dispatch(authActions.login());
+	};
+
+	return (
+		<main className={classes.auth}>
+			<section>
+				<form onSubmit={loginHandler}>
+					<div className={classes.control}>
+						<label htmlFor='email'>Email</label>
+						<input type='email' id='email' />
+					</div>
+					<div className={classes.control}>
+						<label htmlFor='password'>Password</label>
+						<input type='password' id='password' />
+					</div>
+					<button>Login</button>
+				</form>
+			</section>
+		</main>
+	);
 };
 
 export default Auth;
--- src/store/index.js
+++ src/store/index.js
@@ -1,50 +1,14 @@
+/** @format */
 
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+import authReducer from "./authSlice";
 
-const initialState =  { counter: 0 , showCounter:true}
-
-const counterReducer = (state =initialState, action) => {
-	if (action.type == "increment") {
-		return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-		};
-	}
-
-	if (action.type == "increase") {
-		return {
-      counter: state.counter + action.amount,
-       showCounter: state.showCounter,
-		};
-	}
-
-	if (action.type == "decrease") {
-		return {
-      counter: state.counter - action.value,
-       showCounter: state.showCounter,
-		};
-	}
-
-	if (action.type == "decrement") {
-		return {
-      counter: state.counter - 1,
-       showCounter: state.showCounter,
-    };
-  }
-  
-
-  	if (action.type == "toggle") {
-		return {
-      counter: state.counter,
-       showCounter: !state.showCounter,
-		};
-	}
-
-
-
-	return state;
-};
-
-const store = createStore(counterReducer);
+export const store = configureStore({
+	reducer: {
+		counter: counterReducer,
+		auth: authReducer,
+	},
+});
 
 export default store;
